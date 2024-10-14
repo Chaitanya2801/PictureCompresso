5066354155a0ed70da06ab7a79cbe820ea4cc09c
Chaitanya2801 <chetan777agarwal@gmail.com>
2024-10-14 18:28:30 +0530

    react application installed inital commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to PictureCompresso</h1>
+        <p>Upload your CSV file to process images!</p>
+      </header>
+      <main>
+        {/* Upload component will go here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
